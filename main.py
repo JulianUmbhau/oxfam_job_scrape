df038cd080847f2fa4f4415ae2cb61698ecf5979
Julian Jensen <jensen_303@hotmail.com>
2021-08-03 11:15:53 +0200

    updating scrape

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.oxfam.org/en/take-action/jobs/affiliates"
+URL = "https://www.oxfam.org/en/take-action/jobs/affiliates" # page 1 
 
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-result = soup.find("div", class_= "item-list")
+page_list = []
+for a in soup.find_all("a", href=True):
+    if "page" in a["href"]:
+        print(a["href"])
+        page_list.append("https://www.oxfam.org/en/take-action/jobs/affiliates" + a["href"])
 
-results = result.find_all("h3", class_="pb-1")
+job_list = []
+for page in page_list:
+    page = requests.get(page)
+    soup = BeautifulSoup(page.content, "html.parser")
+    result = soup.find("div", class_= "item-list")
+    results = result.find_all("h3", class_="pb-1")
+    for result_i in results:
+        print(result_i.text)
 
-for result_i in results:
-    print(result_i.text)
